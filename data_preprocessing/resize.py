472e01f96089c66a23b71526d084ec077d956bd2
Kokostino <56522863+Kokostino@users.noreply.github.com>
2021-11-02 18:00:06 +0100

    half update

---
+++ data_preprocessing/resize.py
@@ -0,0 +1,52 @@
+import os
+import glob
+from PIL import Image
+
+def resize_and_crop(img_path, size, crop_type='middle'):
+    # If height is higher we resize vertically, if not we resize horizontally
+    img = Image.open(img_path)
+    # Get current and desired ratio for the images
+    img_ratio = img.size[0] / float(img.size[1])
+    ratio = size[0] / float(size[1])
+    #The image is scaled/cropped vertically or horizontally depending on the ratio
+    if ratio > img_ratio:
+        img = img.resize((size[0], int(size[0] * img.size[1] / img.size[0])),
+                Image.ANTIALIAS)
+        # Crop in the top, middle or bottom
+        if crop_type == 'top':
+            box = (0, 0, img.size[0], size[1])
+        elif crop_type == 'middle':
+            box = (0, (img.size[1] - size[1]) / 2, img.size[0], (img.size[1] + size[1]) / 2)
+        elif crop_type == 'bottom':
+            box = (0, img.size[1] - size[1], img.size[0], img.size[1])
+        else :
+            raise ValueError('ERROR: invalid value for crop_type')
+        img = img.crop(box)
+    elif ratio < img_ratio:
+        img = img.resize((size[1] *int( img.size[0] / img.size[1]), size[1]),
+                Image.ANTIALIAS)
+        # Crop in the top, middle or bottom
+        if crop_type == 'top':
+            box = (0, 0, size[0], img.size[1])
+        elif crop_type == 'middle':
+            box = ((img.size[0] - size[0]) / 2, 0, (img.size[0] + size[0]) / 2, img.size[1])
+        elif crop_type == 'bottom':
+            box = (img.size[0] - size[0], 0, img.size[0], img.size[1])
+        else :
+            raise ValueError('ERROR: invalid value for crop_type')
+        img = img.crop(box)
+    else :
+        img = img.resize((size[0], size[1]),
+                Image.ANTIALIAS)
+        # If the scale is the same, we do not need to crop
+    img.save(img_path)
+
+os.chdir(".....\\Palmtree-Classifier\\")
+
+image_mine=glob.glob("images/Favs/*.*")
+image_scraped=glob.glob("images/palmtree/*.*")
+
+image_names=image_mine+image_scraped
+
+for image in image_names:
+    resize_and_crop(image, (100,100))
---
+++ scraping_data/scrape_instagram.py
@@ -0,0 +1,5 @@
+!pip install instagram-scraper==1.7.0
+
+import os
+
+os.popen("instagram-scraper palmtree --tag --maximum 500 -t image -u UUU -p PPP")
